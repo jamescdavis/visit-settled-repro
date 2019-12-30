795e5296e6fe93a155b9eb07879b24cd4010f692
James C. Davis <james@cos.io>
2019-12-30 11:41:51 -0500

    tests: add acceptance test for /

---
+++ tests/acceptance/application-test.js
@@ -0,0 +1,32 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, settled } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+
+module('Acceptance | application', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+
+  test('visiting / - await visit() only', async function(assert) {
+    this.server.createList('foo', 10);
+
+    await visit('/');
+
+    assert.equal(currentURL(), '/');
+    assert.dom('[data-test-title]').exists({ count: 1 });
+    assert.dom('[data-test-list-foos]').exists({ count: 1 });
+    assert.dom('[data-test-foo]').exists({ count: 10 });
+  });
+
+  test('visiting / - with await settled() ', async function(assert) {
+    this.server.createList('foo', 10);
+
+    await visit('/');
+    await settled();
+
+    assert.equal(currentURL(), '/');
+    assert.dom('[data-test-title]').exists({ count: 1 });
+    assert.dom('[data-test-list-foos]').exists({ count: 1 });
+    assert.dom('[data-test-foo]').exists({ count: 10 });
+  });
+});
